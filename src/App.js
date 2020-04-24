f23c2e03652268871cea5153ab40851d760ab9f7
samsung <azamat>
2020-04-24 06:50:46 +0600

    adding header component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import "./App.css";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route path="/shop" component={ShopPage}></Route>
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/original.svg";
+import "./header.styles.scss";
+
+function Header(props) {
+  return (
+    <div className="header">
+      <Link className="logo-container" to="/">
+        <Logo className="logo"></Logo>
+      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+        <Link className="option" to="/contact">
+          CONTACT
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
